c8b4fb5efad5a21c21fa20558d8cc66f466d0d51
Facundo Masgrau <facundomasgrau@gmail.com>
2021-08-23 03:25:23 +0000

    feat(searchbar): add searchbar component

---
+++ src/Components/SearchBar/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface ISearchBarProps {
+    name: string
+    type: string
+    placeholder: string
+    onChange: (data: any) => void
+    value: string
+}
+
+export default function SearchBar({
+    name,
+    type,
+    placeholder,
+    onChange,
+    value,
+}: ISearchBarProps): JSX.Element {
+    return (
+        <>
+            <input
+                id={name}
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                onChange={onChange}
+                value={value}
+            />
+        </>
+    )
+}
