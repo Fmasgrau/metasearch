e50bb073d143f846824734f1d544c6a18df35dcb
Facundo Masgrau <facundomasgrau@gmail.com>
2021-08-25 02:21:17 +0000

    test(e2e): add new test and fix names

--- cypress/integration/searchImages.spec.js
+++ cypress/integration/searchImages.spec.js
@@ -1,44 +1,53 @@
 /* eslint-disable no-undef */
+beforeEach(() => {
+    cy.visit('http://localhost:3000')
+})
+
+const BING_KEY = '095b83c3c7954874b4212b9227c3c442'
+const GOOGLE_KEY = 'AIzaSyAuYucO0jCa1lYfWd235tyJRxb8aGVGKUI'
+const GOOGLE_CX = '4cf935c13c0e105dc'
+const BASE_URL_BING = "https://api.bing.microsoft.com/v7.0/images/search?q="
+const BASE_URL_GOOGLE = "https://customsearch.googleapis.com/customsearch//v1?"
+const SEARCH = 'Facundo'
+const OPTIONS = ['Google', 'Bing', 'Both']
+const SEARCH_TYPE = 'image'
+const RESPONSE_LENGTH = 10
+
+
 describe('Search Images', () => {
-    it('frontpage can be opened', () => {
-        cy.visit('http://localhost:3000')
+    it('Should frontpage can be opened', () => {
         cy.contains('Select Item')
     })
-    it('A successfull search bring me results', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#search').type('Facundo')
-        cy.get('#dropdown').select('Bing')
+    it('Should bring me results', () => {
+        cy.get('#search').type(SEARCH)
+        cy.get('#dropdown').select(OPTIONS[1])
         cy.get('button').click()
-
-
+        cy.get('#card')
     })
-    it('Loading modal opens when I submit a request', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#search').type('Facundo')
-        cy.get('#dropdown').select('Bing')
+    it('Should load modal when I submit a request', () => {
+        cy.get('#search').type(SEARCH)
+        cy.get('#dropdown').select(OPTIONS[1])
         cy.get('button').click()
         cy.get('[data-testid=overlay]')
     })
 
-    it('Get error message when bing api request fail', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#search').type('Facundo')
-        cy.get('#dropdown').select('Bing')
+    it('Should get error message when bing api request fail', () => {
+        cy.get('#search').type(SEARCH)
+        cy.get('#dropdown').select(OPTIONS[1])
         cy.get('button').click()
-        cy.intercept('https://api.bing.microsoft.com/v7.0/images/search?q=Facundo', { forceNetworkError: true })
+        cy.intercept(`${BASE_URL_BING}${SEARCH}`, { forceNetworkError: true })
     })
 
-    it('Get error message when google api request fail', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#search').type('Facundo')
-        cy.get('#dropdown').select('Google')
+    it('Should get error message when google api request fail', () => {
+        cy.get('#search').type(SEARCH)
+        cy.get('#dropdown').select(OPTIONS[0])
         cy.get('button').click()
-        cy.intercept('https://customsearch.googleapis.com/customsearch/v1?key=AIzaSyAuYucO0jCa1lYfWd235tyJRxb8aGVGKUI&cx=4cf935c13c0e105dc&q=Facundo&searchType=image&num=10', { forceNetworkError: true })
+        cy.intercept(`${BASE_URL_GOOGLE}key=${GOOGLE_KEY}cx=${GOOGLE_CX}&q=${SEARCH}&searchType=${SEARCH_TYPE}&num=${RESPONSE_LENGTH}`, { forceNetworkError: true })
     })
     it.only('Should display data from both search engines', () => {
-        cy.visit('http://localhost:3000')
-        cy.get('#search').type('Facundo')
-        cy.get('#dropdown').select('Both')
+        cy.get('#search').type(SEARCH)
+        cy.get('#dropdown').select(OPTIONS[2])
         cy.get('button').click()
+        cy.get('#card')
     })
 })
--- src/Components/ItemList/index.tsx
+++ src/Components/ItemList/index.tsx
@@ -9,7 +9,7 @@ interface IItemListProps {
 export default function ItemList({ url, alt }: IItemListProps): JSX.Element {
     return (
         <>
-            <div className="col-lg-3 col-md-4 col-6">
+            <div className="col-lg-3 col-md-4 col-6" id="card">
                 <div className="h-100 d-flex justify-content-center p-3">
                     <img src={url} alt={alt} className="img-thumbnail" />
                 </div>
